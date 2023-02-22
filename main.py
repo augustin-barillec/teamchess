dcb03d32fd57903da279e8295fbb95e6f1b9f97e
augustinbarillec <augustin.barillec@gmail.com>
2023-02-22 20:22:56 +0100

    login logout

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-import os
 import flask
-import google.cloud.firestore
-import reusable
 
-logger = reusable.root_logger.configure_root_logger()
 app = flask.Flask(__name__)
-# project_id = os.getenv('PROJECT_ID')
-# db = google.cloud.firestore.Client(project=project_id)
+
+app.secret_key = 'asdsdfsdfs13sdf_df%&'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
+    logged = False
+    if 'name' in flask.session:
+        logged = True
+    return flask.render_template('home.html', logged=logged)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if flask.request.method == 'POST':
-        value = flask.request.form['value']
-        return flask.redirect(f'/game_{value}')
-    return flask.render_template('home.html')
+        flask.session['name'] = flask.request.form['name']
+        return flask.redirect('/')
+    return flask.redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    flask.session.pop('name', None)
+    return flask.redirect('/')
 
 
 @app.route('/start')
 def start():
     import random
     x = random.randint(1, 100)
-    print(x)
     return flask.redirect(f'/game_{x}')
+
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if flask.request.method == 'POST':
+        game_id = flask.request.form['game']
+        return flask.redirect(f'/game_{game_id}')
+    return flask.redirect('/')
 
 
 @app.route('/game_<game_id>')
 def game(game_id):
     print(game_id)
-    return flask.render_template('game.html')
+    return flask.render_template('game.html', game_id=game_id)
 
 
 if __name__ == "__main__":
