d31278b12d26e40ff5aacd52844c8e8287f6f902
mOewa <gaetan.mathias@easyence.com>
2023-03-03 11:49:36 +0100

    First try

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+import google.cloud.firestore
+from dotenv import load_dotenv
 
+load_dotenv()
+USERS = 'users_a'
 
 # configuration
 DEBUG = True
+
+# connect to DB
+project_id = os.getenv("PROJECT_ID")
+db = google.cloud.firestore.Client(project=project_id)
 
 # instantiate the app
 app = Flask(__name__)
@@ -16,7 +25,8 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 # sanity check route
 @app.route("/ping", methods=["GET"])
 def ping_pong():
-    return jsonify("pong!")
+    items_ref = db.collection(USERS)
+    return jsonify({item.id: item.to_dict() for item in items_ref.stream()})
 
 
 if __name__ == "__main__":
