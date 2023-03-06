63210f2fe312598007b3d3dbe1f805f0261b12f0
Julien Ledru <julien.ledru@easyence.com>
2023-03-06 23:31:51 +0000

    ongoing

--- models/moves.py
+++ models/moves.py
@@ -1,10 +1,58 @@
-from pydantic import BaseModel
+import re
+from typing import Any, ClassVar, Optional, Pattern
+
+from pydantic import BaseModel, Field
+
+
+class ChessMove(str):
+    """
+    Pydantic field representing a chess move
+    """
+
+    _pattern: ClassVar[str] = r"^[a-h][1-8][a-h][1-8]"
+    _compiled: ClassVar[Pattern] = re.compile(_pattern)
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_regex
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: dict[str, Any]):
+        field_schema.update(
+            title="Chess Move: start position to destination position",
+            type="string",
+            format="move",
+            pattern=cls._pattern,
+            example="a2a4",
+        )
+
+    @classmethod
+    def validate_regex(cls, move: str):
+        if not cls._compiled.fullmatch(move):
+            raise ValueError(f"Invalid Chess Move: '{move!r}', must match pattern: {cls._pattern}")
+        return cls(move)
+
+    def __repr__(self):
+        return f"ChessMove({super().__repr__()})"
 
 
 class PlayerMove(BaseModel):
     player_id: str
-    move: str
+    move: ChessMove
 
 
 class MovesPayload(BaseModel):
     moves: list[PlayerMove]
+
+
+class ChessMoves(BaseModel):
+    list_moves: list[ChessMove]
+
+
+class MoveEval(BaseModel):
+    move: ChessMove = Field(alias="Move")
+    centipawn: int = Field(alias="Centipawn")
+    mate_in: Optional[int] = Field(alias="Mate")
+
+    class Config:
+        allow_population_by_field_name = True
--- services/stockfish_services.py
+++ services/stockfish_services.py
@@ -1,13 +1,29 @@
 from stockfish import Stockfish
 
-from models.configuration import StockfishConf, GcpConf
+from models.configuration import StockfishConf
+from models.moves import ChessMoves, MoveEval
 
-gcp_conf = GcpConf.load()
 
-# stock_conf = StockfishConf.load()
+class StockfishServices:
+    def __init__(self) -> None:
+        stock_conf = StockfishConf.load()
+        self.stockfish = Stockfish(
+            path=stock_conf.path,
+            depth=stock_conf.depth,
+            parameters={"Threads": stock_conf.threads, "Minimum Thinking Time": stock_conf.min_thinking_time},
+        )
 
-# stockfish = Stockfish(
-#     path=stock_conf.path,
-#     depth=stock_conf.depth,
-#     parameters={"Threads": stock_conf.threads, "Minimum Thinking Time": stock_conf.min_thinking_time},
-# )
+    def _set_position(self, moves: ChessMoves) -> None:
+        self.stockfish.set_position(moves.list_moves)
+
+    def get_top_moves(self, nb: int) -> list[MoveEval]:
+        return [MoveEval(**move_eval) for move_eval in self.stockfish.get_top_moves(nb)]
+
+
+sf = StockfishServices()
+
+sf._set_position(ChessMoves(list_moves=["a2a4", "e7e5"]))
+
+top_moves = sf.get_top_moves(10)
+
+print(top_moves)
